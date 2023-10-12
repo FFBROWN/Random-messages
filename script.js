c257182ffc20c01ee4450d1aae763fbc924ded1a
Vincent Brown <vincentsrealty@gmail.com>
2023-10-11 20:11:29 -0500

    js generates random message each time program runs

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-console.log("Hello")
-console.log("Hello")
+const books =["John","Peter","Acts","Luke"];
+const chapter =["one","two","three","four"];
+const verse = [1,2,3,4]
 
+const randomMessage =()=>{
+
+    const randomBooks =books[Math.floor(Math.random()* books.length)]
+    const randomChapter =chapter[Math.floor(Math.random()* chapter.length)]
+    const randomVerse =verse[Math.floor(Math.random()* verse.length)]
+   return `${randomBooks},${randomChapter},${randomVerse}`
+
+};
+console.log(randomMessage())
+
+const rm = randomMessage();
+console.log(rm)
 
 
 
